14e088371333acf4453f0850a1e765a586805747
zhangbo2008 <15122306087@163.com>
2020-02-01 21:10:33 +0800

    1

--- contents/1_command_line_reinforcement_learning/treasure_on_right.py
+++ contents/1_command_line_reinforcement_learning/treasure_on_right.py
@@ -4,6 +4,10 @@ An agent "o" is on the left of a 1 dimensional world, the treasure is on the rig
 Run this program and to see how the agent will improve its strategy of finding the treasure.
 
 View more on my tutorial page: https://morvanzhou.github.io/tutorials/
+
+从莫凡python开始学习rl.2020-02-01,14点05
+有代码有视频,有公式.必须靠谱的教程.把学习过程直接写到这套代码的注释里面.
+
 """
 
 import numpy as np
@@ -24,7 +28,8 @@ FRESH_TIME = 0.3    # fresh time for one move
 
 def build_q_table(n_states, actions):
     table = pd.DataFrame(
-        np.zeros((n_states, len(actions))),     # q_table initial values
+        np.zeros((n_states, len(actions))),     # q_table initial values 初始化全是0
+        #因为默认是没有value的.没有奖励和惩罚.
         columns=actions,    # actions's name
     )
     # print(table)    # show table
@@ -33,7 +38,7 @@ def build_q_table(n_states, actions):
 
 def choose_action(state, q_table):
     # This is how to choose an action
-    state_actions = q_table.iloc[state, :]
+    state_actions = q_table.iloc[state, :]   #当前状态的action表.也就是q表的其中一行.
     if (np.random.uniform() > EPSILON) or ((state_actions == 0).all()):  # act non-greedy or state-action have no value
         action_name = np.random.choice(ACTIONS)
     else:   # act greedy
@@ -44,7 +49,7 @@ def choose_action(state, q_table):
 def get_env_feedback(S, A):
     # This is how agent will interact with the environment
     if A == 'right':    # move right
-        if S == N_STATES - 2:   # terminate
+        if S == N_STATES - 2:   # terminate     0 ,1,2,3,4,5
             S_ = 'terminal'
             R = 1
         else:
@@ -74,19 +79,69 @@ def update_env(S, episode, step_counter):
         time.sleep(FRESH_TIME)
 
 
+
+
+
+#核心就是rl函数!!!!!!!!!!!!!!!!!!!也是程序的入口
 def rl():
-    # main part of RL loop
+    # main part of RL loop  建立初始化环境.
     q_table = build_q_table(N_STATES, ACTIONS)
+
+
+
     for episode in range(MAX_EPISODES):
         step_counter = 0
         S = 0
         is_terminated = False
-        update_env(S, episode, step_counter)
-        while not is_terminated:
+        update_env(S, episode, step_counter) #利用这个update_env函数来画出来s的当前状态. 也就是可视化函数.
+        while not is_terminated:  # 到达目标就停止这次迭代
 
-            A = choose_action(S, q_table)
+            A = choose_action(S, q_table) #根据 一部禧龙-贪婪的方法根据q表选一个action.
+
+
+            #根据s和a计算出s',这里面就是s_   ,表示a作用后得到的新s 和对应的r ,r 这个函数是需要人工
+            #制定的. 这里面s',到terminal 是1, 其他都是0.
             S_, R = get_env_feedback(S, A)  # take action & get next state and reward
+
+
+
+            q_predict = q_table.loc[S, A] # q预测就是当前q表给的数值.
+
+
+            #下面计算q真实: q_真实=当前r+ 未来衰减系数* 未来收获的max
+
+
+            # 为什么这么算是对的呢??
+
+            '''
+            
+              q_target = R + GAMMA * q_table.iloc[S_, :].max() 
+              核心就是这行代码.
+              表示了q_真实应该这么算.为什么呢?
+              这行本质是按照Gamma概率选择q表中S_对应的action中Q值最大的action,1-Gamma概率选择其他action.
+              所以是局部贪心.
+              证明的核心是要证明,是否能让最后一步得到的-1.体现在之前的每一步中.
+              1.假设S_是terminal,对应奖励是给1,所以奖励通过公式传递给了q_target,让算法提前一步
+              知道了这么走是对的.因为这么走的q_target变大了.
+              2.R是表示当前奖励,
+              
+              
+              
+            
+              
+              
+              
             q_predict = q_table.loc[S, A]
+            
+            
+            '''
+
+
+
+
+
+
+
             if S_ != 'terminal':
                 q_target = R + GAMMA * q_table.iloc[S_, :].max()   # next state is not terminal
             else:
